2d2b726524f4145a0c1bdf2fab1105d6c7625309
abanerjee-06 <aritra.banerjee@students.iiit.ac.in>
2021-12-16 10:22:11 +0300

    Preparing the repository to include other projects

--- create_csv.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-
-quiz_id = [1,2,3]
-quiz_name = ['Sorting Algorithms(General)','Quick Sort','Time Complexity of Algorithms']
-
-D = {'quiz_id':quiz_id,'quiz_name':quiz_name}
-df = pd.DataFrame(D)
-df.to_csv('quiz.csv')
